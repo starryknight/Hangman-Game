b48efe518775053b4bcf8384224c79f3f57a21a5
starryknight <mrstanleykimani@gmail.com>
2018-05-21 20:16:14 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Hangman-Game
 This is a hangman game app
-
+#instructions
+#trello(user story)
 Trello link https://trello.com/b/ZC9VWd0p/game-project
-
+#wireframe
 wireframe on figma https://www.figma.com/file/a3ZEzfQktqMyWjSPgynokAMH/hangman
+#resources
--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 // var notification = alertify.notify('sample', 'success', 5, function(){  console.log('dismissed'); });
 
-console.log('new')
+
 var arrayOfWords = ["war", "cook", "reign", "bushes", "dark", "profuse", "uttermost", "coil", "snail", "sense", "trite", "divide", "fetch", "unadvised", "protective", "bike", "snow", "lake", "stop", "sort", "imminent", "waves", "dusty", "blade", "slave", "poke" , "duck", "parsimonious", "street"]//random words found on https://www.randomlists.com/random-words
 
 $(document).ready(function(){
-
-    var newWord = arrayOfWords[Math.floor(Math.random()*arrayOfWords.length)]
-    console.log(newWord) 
+    
+    var currentWord = arrayOfWords[Math.floor(Math.random()*arrayOfWords.length)]
+    var playWord =[]
+    console.log(currentWord) 
+    
+    $('btn-success').click(function(){
+        console.log('new')
+    });
  });
